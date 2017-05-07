761237d3690b2729ef26ab136bd86e95a2c36e88
Ayumi <ayumihosoda@gmail.com>
2017-05-06 23:36:53 -0700

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,63 @@
 var signs = [
 {
 	sign: "Aries",
-	trait: "Thoughtful"
+	image: "images/1.png",
+	trait: "Courageous, determined, confident, enthusiastic, optimistic, honest, passionate"
 },
 {
 	sign: "Taurus",
-	trait: "Patience"
+	image: "images/2.png",
+	trait: "Reliable, patient, practical, devoted, responsible, stable"
 },
 {
 	sign: "Gemini",
-	trait: "Intelligent"
+	image: "images/3.png",
+	trait: "Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas"
 },
 {
 	sign: "Cancer",
-	trait: "Thoughtful"
+	image: "images/4.png",
+	trait: "Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive"
 },
 {
 	sign: "Leo",
-	trait: "Thoughtful"
+	image: "images/5.png",
+	trait: "Creative, passionate, generous, warm-hearted, cheerful, humorous"
 },
 {
 	sign: "Virgo",
-	trait: "Thoughtful"
+	image: "images/6.png",
+	trait: "Loyal, analytical, kind, hardworking, practical"
 },
 {
 	sign: "Libra",
-	trait: "Thoughtful"
+	image: "images/7.png",
+	trait: "Cooperative,diplomatic, gracious, fair-minded, social"
 },
 {
 	sign: "Scorpio",
-	trait: "Thoughtful"
+	image: "images/8.png",
+	trait: "Resourceful, brave, passionate, stubborn, a true friend"
 },
 {
 	sign: "Sagittarius",
-	trait: "Thoughtful"
+	image: "images/9.png",
+	trait: "Generous, idealistic, great sense of humor"
 },
 {
 	sign: "Capricorn",
-	trait: "Thoughtful"
+	image: "images/10.png",
+	trait: "Responsible, disciplined, self-control, good managers"
 },
 {
 	sign: "Aquarius",
-	trait: "Thoughtful"
+	image: "images/11.png",
+	trait: "Progressive, original, independent, humanitarian"
 },
 {
 	sign: "Pisces",
-	trait: "Thoughtful"
+	image: "images/12.png",
+	trait: "Compassionate, artistic, intuitive, gentle, wise, musical"
 }
 ]
 
@@ -53,7 +65,8 @@ function getInfo() {
 	var horoscope = document.getElementById("horoscope").value
 	for (i=0; i < signs.length; i++) {
 		if (signs[i].sign == horoscope) {
-			document.getElementById("sign").innerHTML = "<h2>" + signs[i].sign + "!" + "</h2>"
+			document.getElementById("sign").innerHTML = "<h2>" + signs[i].sign + "</h2>"
+			document.getElementById("image").src = signs[i].image
 			document.getElementById("trait").innerHTML = "<p>" + signs[i].trait + "</p>"
 			return
 		}
